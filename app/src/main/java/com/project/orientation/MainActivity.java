7e94b73403fafe73a699e03a1ffe5fa478bbff62
Yaqub <yaqubpatel1484@gmail.com>
2020-10-12 07:42:42 +0530

    Something changed

--- app/src/main/java/com/project/orientation/MainActivity.java
+++ app/src/main/java/com/project/orientation/MainActivity.java
@@ -13,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e("MainActivity","*************************************");
+
         Log.e("MainActivity","OnCreate");
         TextView tv = findViewById(R.id.tv);
         tv.setOnClickListener(new View.OnClickListener() {
